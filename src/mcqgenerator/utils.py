7ceb95a349185f5c96c27c67a5ab3c5a3d4f0508
Muhammd-waleed <109720980+Muhammd-waleed@users.noreply.github.com>
2024-04-27 10:15:48 +0500

    Update utils.py

--- src/mcqgenerator/utils.py
+++ src/mcqgenerator/utils.py
@@ -1,5 +1,5 @@
 import os
-import PyPDF2
+from PyPDF2 import PdfFileReader
 import json
 import traceback
 
@@ -10,7 +10,7 @@ import traceback
 def  read_file(file):
     if file.name.endswith(".pdf"):
         try:
-            pdf_reader=PyPDF2.PdfFileReader(file)
+            pdf_reader=PdfFileReader(file)
             text=""
             for page in pdf_reader.pages:
                 text+=page.extract_text()
